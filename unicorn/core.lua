8675c20532e44eb17f680e6467ea1409f9e80b24
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-10-30 17:41:03 -0700

    Fix GitHub Gist provider

--- unicorn/core.lua
+++ unicorn/core.lua
@@ -92,7 +92,7 @@ local function install_gist(package_table)
 	-- this is really simple, only works predictably with a one-file gist.
 	for k,v in pairs(package_table.instdat.filemaps) do
 		-- uses source code repository-like names because a Gist is a repository technically :)
-		local http_data = unicorn.util.smartHttp("https://gist.githubusercontent.com/" .. package_table.instdat.repo_owner .."/"..  package_table.instdat.repo_name .."/raw/"..package_table.repo_ref.."/"..k)
+		local http_data = unicorn.util.smartHttp("https://gist.githubusercontent.com/" .. package_table.instdat.repo_owner .."/"..  package_table.instdat.repo_name .."/raw/"..package_table.instdat.repo_ref.."/"..k)
 		unicorn.util.fileWrite(http_data, v)
 	end
 end
