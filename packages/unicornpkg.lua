f9093cc3f90853652ed1614e705b29ebfcd01a30
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-01 18:05:24 -0700

    Add packages

---
+++ packages/unicornpkg.lua
@@ -0,0 +1,17 @@
+fs.makeDir("/lib/unicorn")
+
+local package = {}
+package.name = "unicornpkg"
+package.instdat = {}
+package.instdat.repo_owner = "unicornpkg"
+package.instdat.repo_name = "unicornpkg"
+package.instdat.repo_ref = "0.1.0"
+package.instdat.filemaps = {}
+package.instdat.filemaps["unicorn/init.lua"] = "/lib/unicorn/init.lua"
+package.instdat.filemaps["unicorn/core.lua"] = "/lib/unicorn/core.lua"
+package.instdat.filemaps["unicorn/util.lua"] = "/lib/unicorn/util.lua"
+package.pkgType = "com.github"
+package.unicornSpec = "v1.0.0"
+package.version = "0.1.0"
+
+return package
