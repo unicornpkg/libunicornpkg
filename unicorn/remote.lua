6f09741aeadcdb6f223dbb7ba780184a99e86dd0
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-02 21:04:24 -0700

    Working prototype

--- unicorn/remote.lua
+++ unicorn/remote.lua
@@ -9,7 +9,7 @@ unicorn.remote = {}
 function unicorn.remote.install(package_name)
 	for _,v in pairs(fs.list("/etc/unicorn/remotes/")) do
 		local v = fs.open("/etc/unicorn/remotes/"..v, "r")
-		local v = v.readAll()
+		local v = v.readLine()
 		local v = v:gsub("https://", "") -- have to remove the https:// prefix because fs.combine does weird stuff with it if it's left in
 		local v = fs.combine(v, package_name..".lua")
 		v = 'https://'..v
@@ -21,8 +21,8 @@ function unicorn.remote.install(package_name)
 				error("HTTP request to "..v.." failed with error "..httpError)
 			end
 		end
-		local package_table = loadstring(response)
-		unicorn.core.install(package_table)
+		unicorn.util.fileWrite(response, "/tmp/"..package_name)
+		unicorn.core.install(dofile("/tmp/"..package_name))
 	end
 end
 
