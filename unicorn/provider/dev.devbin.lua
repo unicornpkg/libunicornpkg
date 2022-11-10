5b12c07d044ef699c881b8ca9266dc2cf34c4fca
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-11-10 12:26:32 -0800

    Deprecate dev.devbin.lua

--- unicorn/provider/dev.devbin.lua
+++ unicorn/provider/dev.devbin.lua
@@ -2,6 +2,7 @@ local unicorn = dofile("/lib/unicorn/init.lua")
 
 --- Package provider for Devbin.dev.
 -- @param package_table table A valid package table
+-- @deprecated because devbin.dev has shut down.
 local function install_devbin(package_table)
 	for k, v in pairs(package_table.instdat.filemaps) do
 		local http_data = unicorn.util.smartHttp("https://devbin.dev/raw/" .. k)
