34b41817f6b9507b871141c1d18ce69edb969f73
red <lukas.fricke.dev@gmail.com>
2022-12-26 13:11:48 +0100

    Ignore dirs

--- unicorn/remote.lua
+++ unicorn/remote.lua
@@ -12,33 +12,35 @@ function unicorn.remote.install(package_name)
 		-- TODO: Change the variable names into something more descriptive
 		-- TODO: Split this into smaller local functions
 		for _, v0 in pairs(fs.list("/etc/unicorn/remotes/")) do
-			local v1 = fs.open("/etc/unicorn/remotes/" .. v0, "r")
-			local v2 = v1.readLine()
-			local v3 = v2:gsub("https://", "") -- have to remove the https:// prefix because fs.combine does weird stuff with it if it's left in
-			local v4 = fs.combine(v3, package_name .. ".lua")
-			local v5 = "https://" .. v4
-			local response, httpError = unicorn.util.smartHttp(v5)
-			if httpError then
-				if not httpError == "Not Found" then
-					error("HTTP request to " .. v5 .. " failed with error " .. httpError)
-				end
-			else
-				unicorn.util.fileWrite(response, "/tmp/" .. package_name)
-				print(response)
-				print(httpError)
-
-				local package_table = loadstring(response)()
-
-				-- install depends
-				if package_table.rel ~= nil and package_table.rel.depends ~= nil then
-					for _, package in pairs(package_table.rel.depends) do
-						unicorn.remote.install(package)
+			if not fs.isDir("/etc/unicorn/remotes/" .. v0) then
+				local v1 = fs.open("/etc/unicorn/remotes/" .. v0, "r")
+				local v2 = v1.readLine()
+				local v3 = v2:gsub("https://", "") -- have to remove the https:// prefix because fs.combine does weird stuff with it if it's left in
+				local v4 = fs.combine(v3, package_name .. ".lua")
+				local v5 = "https://" .. v4
+				local response, httpError = unicorn.util.smartHttp(v5)
+				if httpError then
+					if not httpError == "Not Found" then
+						error("HTTP request to " .. v5 .. " failed with error " .. httpError)
 					end
+				else
+					unicorn.util.fileWrite(response, "/tmp/" .. package_name)
+					print(response)
+					print(httpError)
+
+					local package_table = loadstring(response)()
+
+					-- install depends
+					if package_table.rel ~= nil and package_table.rel.depends ~= nil then
+						for _, package in pairs(package_table.rel.depends) do
+							unicorn.remote.install(package)
+						end
+					end
+
+					unicorn.core.install(package_table)
+
+					downloaded = true
 				end
-
-				unicorn.core.install(package_table)
-
-				downloaded = true
 			end
 		end
 	end
