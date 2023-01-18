f67390408131e15f77711c4ac45bc1bc9f42295d
Tomo <68489118+tomodachi94@users.noreply.github.com>
2023-01-18 10:52:35 -0800

    provider(io.sc3.p): Add SC3 Pastebin provider

---
+++ unicorn/provider/io.sc3.p.lua
@@ -0,0 +1,12 @@
+local unicorn = dofile("/lib/unicorn/init.lua")
+
+--- Package provider for Pastebin.com.
+-- @param package_table table A valid package table
+local function install_sc3_paste(package_table)
+	for remote_path, install_path in pairs(package_table.instdat.filemaps) do
+		local http_data = unicorn.util.smartHttp(("https://p.sc3.io/api/v1/pastes/%s/raw"):format(remote_path))
+		unicorn.util.fileWrite(http_data, install_path)
+	end
+end
+
+return install_sc3_paste
