3a1b0a3d2faf041a662e5950da5ea56d1369e632
Tomodachi94 <tomodachi94@protonmail.com>
2025-02-08 00:20:35 +0000

    add a nix shell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2024 awesome-computercraft contributors
+#
+# SPDX-License-Identifier: MIT
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { self, nixpkgs, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = pkgs.mkShellNoCC {
+              packages = with pkgs; [
+                just
+              ];
+            };
+          });
+    };
+}
