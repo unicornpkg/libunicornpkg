4bb18f62940ff7fa8fbe024ef4acc9fe94a2de20
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-04 23:54:38 +0000

    test(provider.local.string): init

--- test/unicorn/provider_local_string_spec.lua
+++ test/unicorn/provider_local_string_spec.lua
@@ -4,4 +4,24 @@ describe("provider.local.string", function()
    it("require('unicorn.provider.local.string') returns a function", function()
      expect(require("unicorn.provider.local.string")):type("function")
    end)
+   it("can be installed and uninstalled", function()
+    local unicornCore = require("unicorn.core")
+    --local testutils = require("testutils")
+
+    local package = {}
+    package.pkgType = "local.string"
+    package.unicornSpec = "v1.0.0"
+    package.name = "test-local.string"
+    package.version = "0.0.1"
+    package.instdat = {}
+    package.instdat.filemaps = {}
+    package.instdat.filemaps["return 1"] = "/lib/local-string-test-foo.lua"
+    package.instdat.filemaps["return 2"] = "/lib/local-string-test-bar.lua"
+
+    expect(unicornCore.install(package)):equals(true)
+    expect(fs.exists("/etc/unicorn/packages/installed/test-local.string")):equals(true)
+    expect(require("local-string-test-foo")):equals(1)
+    expect(require("local-string-test-bar")):equals(2)
+    expect(unicornCore.uninstall("test-local.string")):equals(true)
+  end)
 end)
