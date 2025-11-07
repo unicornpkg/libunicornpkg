a41beaf4a14ef19ed36d820f0be3c96c0511e6d4
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-07 07:40:50 +0000

    docs: add stuff to entrypoint

--- unicorn/init.lua
+++ unicorn/init.lua
@@ -1,5 +1,16 @@
 package.path = "/lib/?.lua;/lib/?;/lib/?/init.lua;" .. package.path
 
+--- Using
+--- -----
+---
+--- libunicornpkg should be loaded using ``require``.
+--- If you are not familiar with ``require``, see `a guide on tweaked.cc <https://tweaked.cc/guide/using_require.html>`_ for more information.
+--- libunicornpkg should have been installed to the ``/lib`` directory,
+--- so a standard ``require`` doesn't usually work here.
+--- You'll need to modify ``package.path`` so that ``require`` knows where to look for libunicornpkg:
+---
+--- >>> package.path = "/lib/?.lua;/lib/?;/lib/?/init.lua;" .. package.path
+---
 --- !doctype module
 --- @class unicorn
 local unicorn = {}
