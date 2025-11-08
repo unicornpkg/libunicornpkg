80115050efa92143e048eb652289149e89e44c99
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-08 21:59:23 +0000

    test(provider.com.github): init

---
+++ test/unicorn/provider_com_github_spec.lua
@@ -0,0 +1,27 @@
+package.path = "/lib/?.lua;/lib/?;/lib/?/init.lua;" .. package.path
+
+describe("provider.com.github", function()
+	it("require('unicorn.provider.com.github') returns a function", function()
+		expect(require("unicorn.provider.com.github")):type("function")
+	end)
+	it("can be installed and uninstalled", function()
+		local unicornCore = require("unicorn.core")
+
+		local thisPackage = {}
+		thisPackage.pkgType = "com.github"
+		thisPackage.unicornSpec = "v1.0.0"
+		thisPackage.name = "com-github-test"
+		thisPackage.version = "0.0.1"
+		thisPackage.instdat = {}
+		thisPackage.instdat.repo_owner = "unicornpkg"
+		thisPackage.instdat.repo_name = "test-repo"
+		thisPackage.instdat.repo_ref = "caaea67460e1c3b8e275d83500bb1100c3a1837c"
+		thisPackage.instdat.filemaps = {}
+		thisPackage.instdat.filemaps["com-github-test.lua"] = "/lib/com-github-test.lua"
+
+		expect(unicornCore.install(thisPackage)):equals(true)
+		expect(fs.exists("/etc/unicorn/packages/installed/" .. thisPackage.name)):equals(true)
+		expect(require(thisPackage.name)):equals(1)
+		expect(unicornCore.uninstall(thisPackage.name)):equals(true)
+	end)
+end)
