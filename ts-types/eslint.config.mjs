a90e06c75bab13318c9f6f4891bed5c6fbd7f76d
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-18 06:52:43 +0000

    chore(ts-types): migrate to eslint flat config

---
+++ ts-types/eslint.config.mjs
@@ -0,0 +1,19 @@
+import eslintRecommended from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...eslintRecommended.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+];
