187b8ca74f4069183624d4d725af26ca162937ed
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-19 05:06:43 +0000

    chore(format): apply prettier

--- ts-types/unicorn.d.ts
+++ ts-types/unicorn.d.ts
@@ -2,37 +2,42 @@
 /** @noResolution **/
 
 export interface PackageTableType {
-    // v1.0.0 is what libunicornpkg currently supports
-    unicornSpec: "v1.0.0";
-    pkgType: "com.github" | "com.github.releases" | "com.gitlab" | "com.pastebin" | "org.bitbucket";
-    name: string;
-    desc?: string;
-    maintainer?: string;
-    licensing?: string;
-    version?: string;
-    script?: {
-        preinstall?: string;
-        postinstall?: string;
-        preremove?: string;
-        postremove?: string;
-    };
-    rel?: {
-        depends?: string[];
-        conflicts?: string[];
-    };
-    security?: { sha256?: Map<string, string> };
+  // v1.0.0 is what libunicornpkg currently supports
+  unicornSpec: "v1.0.0";
+  pkgType:
+    | "com.github"
+    | "com.github.releases"
+    | "com.gitlab"
+    | "com.pastebin"
+    | "org.bitbucket";
+  name: string;
+  desc?: string;
+  maintainer?: string;
+  licensing?: string;
+  version?: string;
+  script?: {
+    preinstall?: string;
+    postinstall?: string;
+    preremove?: string;
+    postremove?: string;
+  };
+  rel?: {
+    depends?: string[];
+    conflicts?: string[];
+  };
+  security?: { sha256?: Map<string, string> };
 }
 
 declare module "unicorn" {
-    export namespace core {
-        export function install(package: PackageTableType): boolean;
-        export function uninstall(package_name: string): boolean;
-    }
-    export namespace remote {
-        export function install(): (pcm: string[]) => boolean;
-    }
-    export namespace util {
-        export function smartHttp(): (sUrl: string) => string;
-        export function fileWrite(): (sContent: string, sPath: string) => boolean;
-    }
+  export namespace core {
+    export function install(package: PackageTableType): boolean;
+    export function uninstall(package_name: string): boolean;
+  }
+  export namespace remote {
+    export function install(): (pcm: string[]) => boolean;
+  }
+  export namespace util {
+    export function smartHttp(): (sUrl: string) => string;
+    export function fileWrite(): (sContent: string, sPath: string) => boolean;
+  }
 }
