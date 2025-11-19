3006f5dcfec81faa30aac717e5047c7e0a7e65ba
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-19 19:45:44 +0000

    chore(constants): fix formatting

--- unicorn/constants.lua
+++ unicorn/constants.lua
@@ -4,6 +4,6 @@ unicorn.constants = {}
 unicorn.constants.projectName = "libunicornpkg"
 unicorn.constants.version = "1.3.1"
 unicorn.constants.infoLink = "https://unicornpkg.github.io/libunicornpkg/information-for-websites"
-unicorn.constants.userAgent = table.concat({projectName, version, infoLink}, " ")
+unicorn.constants.userAgent = table.concat({ projectName, version, infoLink }, " ")
 
 return unicorn.constants
