fe1a692f123298ed41227f5dd8176e6c3feab870
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-21 05:03:28 +0000

    test(cli_hoof): init
    Closes #79

---
+++ test/unicorn/cli_hoof_spec.lua
@@ -0,0 +1,27 @@
+describe("hoof", function()
+	it("can install a package from a file", function()
+		local unicornCore = require("unicorn.core")
+		expect(shell.run("/bin/hoof install string_pack")):equals(true)
+
+		expect(fs.exists("/etc/unicorn/packages/installed/string_pack")):equals(true)
+		expect(require("string_pack").pack):type("function")
+		unicornCore.uninstall("string_pack")
+	end)
+	it("can uninstall a package", function()
+		local unicornCore = require("unicorn.core")
+
+		local thisPackage = {}
+		thisPackage.pkgType = "local.string"
+		thisPackage.unicornSpec = "v1.0.0"
+		thisPackage.name = "hoof-uninstall-test"
+		thisPackage.version = "0.0.1"
+		thisPackage.instdat = {}
+		thisPackage.instdat.filemaps = {}
+		thisPackage.instdat.filemaps["return 1"] = "/lib/hoof-uninstall-test.lua"
+
+		unicornCore.install(thisPackage)
+		expect(shell.run("/bin/hoof uninstall hoof-uninstall-test")):equals(true)
+		expect(fs.exists("/etc/unicorn/packages/installed/hoof-uninstall-test")):equals(false)
+		expect.error(require, "hoof-uninstall-test")
+	end)
+end)
