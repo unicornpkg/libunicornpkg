d88420350827918a9bd949b52215ea6d31f6e693
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-21 21:12:56 +0000

    test(startup): wrap everything except shutdown in pcall
    So that failures don't cause zombie instances of
    CraftOS-PC to linger and waste CI minutes.

--- test/startup.lua
+++ test/startup.lua
@@ -1,9 +1,11 @@
-package.path = "/lib/?.lua;/lib/?;/lib/?/init.lua;" .. package.path
-local unicorn = require("unicorn")
-unicorn.remote.install("semver")
-unicorn.remote.install("ccryptolib")
-unicorn.remote.install("mcfly")
+pcall(function()
+	package.path = "/lib/?.lua;/lib/?;/lib/?/init.lua;" .. package.path
+	local unicorn = require("unicorn")
+	unicorn.remote.install("semver")
+	unicorn.remote.install("ccryptolib")
+	unicorn.remote.install("mcfly")
+	_G.UNICORN_DEBUG_DO_NOT_USE_IN_PRODUCTION_CODE = 1
+	shell.run("bin/mcfly.lua source/test/unicorn/")
+end)
 
-_G.UNICORN_DEBUG_DO_NOT_USE_IN_PRODUCTION_CODE = 1
-shell.run("bin/mcfly.lua source/test/unicorn/")
 os.shutdown()
