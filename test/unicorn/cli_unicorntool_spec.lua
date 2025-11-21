791f69970daa642a62db704b73d94826e7f0b89e
Tomodachi94 <tomodachi94@protonmail.com>
2025-11-21 04:57:51 +0000

    test(cli_unicorntool): init
    Part of #79

--- test/unicorn/cli_unicorntool_spec.lua
+++ test/unicorn/cli_unicorntool_spec.lua
@@ -1,15 +1,27 @@
 describe("unicorntool", function()
-	pending("can install a package table", function()
-		expect(os.run({}, "/bin/unicorntool", "install", "source/test/data/unicorntool-install-test-package.lua")):equals(
-			true
-		)
+	it("can install a package from a file", function()
+		local unicornCore = require("unicorn.core")
+		expect(shell.run("/bin/unicorntool install source/test/data/unicorntool-install-test-package.lua")):equals(true)
 
 		expect(fs.exists("/etc/unicorn/packages/installed/unicorntool-install-test")):equals(true)
-		expect(require("unicorntool-install-test-package")):equals(1)
+		expect(require("unicorntool-install-test")):equals(1)
+		unicornCore.uninstall("unicorntool-install-test")
 	end)
-	pending("can uninstall a package", function()
-		expect(os.run({}, "/bin/unicorntool", "uninstall", "unicorntool-install-test")):equals(true)
-		expect(fs.exists("/etc/unicorn/packages/installed/unicorntool-install-test")):equals(false)
-		expect.error(require, "unicorntool-install-test")
+	it("can uninstall a package", function()
+		local unicornCore = require("unicorn.core")
+
+		local thisPackage = {}
+		thisPackage.pkgType = "local.string"
+		thisPackage.unicornSpec = "v1.0.0"
+		thisPackage.name = "unicorntool-uninstall-test"
+		thisPackage.version = "0.0.1"
+		thisPackage.instdat = {}
+		thisPackage.instdat.filemaps = {}
+		thisPackage.instdat.filemaps["return 1"] = "/lib/unicorntool-uninstall-test.lua"
+
+		unicornCore.install(thisPackage)
+		expect(shell.run("/bin/unicorntool uninstall unicorntool-uninstall-test")):equals(true)
+		expect(fs.exists("/etc/unicorn/packages/installed/unicorntool-uninstall-test")):equals(false)
+		expect.error(require, "unicorntool-uninstall-test")
 	end)
 end)
